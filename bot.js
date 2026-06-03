099d6590562a51cfc5a971a7d1f32768541f1aa5
ramakisahib4-arch <ramakisahib4@gmail.com>
2026-06-03 09:13:05 +0430

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "ربات شما فعال است 24/7 ✅");
+});
